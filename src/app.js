b49be3983dddd9ec0d80581043ae8824ba1307fa
pabitra <pabitramallik81@gmail.com>
2025-04-23 06:34:34 +0530

    exploring

--- src/app.js
+++ src/app.js
@@ -1,29 +1,26 @@
 const express = require('express');
 
 const app = express();
-app.use('/test',(req, res, next) => {
 
-res.send("hello from server");
+
+
+app.use('/user', (req, res, next) => {
+    // console.log('Middleware for /user route');
+    res.send('Middleware response for /user route');
+    next(); // Call the next middleware or route handler
+})
+app.get('/user', (req, res) => {
+res.send({name:"John Doe", age: 30, email:"xyz"} )
+})
+app.post('/user', (req, res) => {
+    res.send("data saved to database ")
+})
     
-    
-    
+app.use('/test',(req, res) => {
+res.send("test from server");
+
 })
 
-
-hello = (req, res, next) => {
-    res.send("hello from app.js");
-}
-
-console.log(hello.toString());
-
-
-app.use('/hello',(req, res, next) => {
-
-    res.send("hello hellwo from server");
-        
-        
-        
-    })
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
