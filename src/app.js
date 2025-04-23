fa0db523771ddbca3bbb571a374fc82b5c55b3b4
pabitra <pabitramallik81@gmail.com>
2025-04-23 21:04:22 +0530

    route handler

--- src/app.js
+++ src/app.js
@@ -5,13 +5,16 @@ const app = express();
 
 
 
-app.get(/u/, (req, res) => {
-    console.log(req.query);
+app.use("/user", (req, res,next) => {
+    res.send("User route is working!");
+    console.log("User route is working!");
+    next();
     
-res.send({name:"John Doe", age: 30, email:"xyz"} )
-})
-
-
+});
+app.use("/product", (req, res,next) => {
+   res.send("Product route is working!");
+    console.log("Product route is working!");
+});
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
     
