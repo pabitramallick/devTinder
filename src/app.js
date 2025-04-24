32460e0c6c541bd874b989f74105d784c886315f
pabitra <pabitramallik81@gmail.com>
2025-04-24 07:00:53 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
 const express = require('express');
 
 const app = express();
+app.get('/x', (req, res) => {
 
-const { adminAuth, userAuth } = require('./middlewares/auth'); 
-
-app.use('/admin', adminAuth); 
-
-
-app.get("/normalPage",userAuth, (req, res) => {
-console.log("Normal Page Middleware");
+try {
+    throw new Error('This is an error!');
+    res.send('Data send');
+} catch (error) { 
+    console.error('Error occurred:', error);
+    res.status(500).send('Internal Server Error');
     
-res.send("Normal Page");
-})
+    
+}
 
-app.get("/admin/user", (req, res) => {
-    res.send("Admin Page");
-})
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
